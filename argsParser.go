b3115d54de5d5efccd01894672f853a624ffe1b9
e-aub <eaub@proton.me>
2024-10-26 07:40:58 +0100

    handle flags with two dashes

--- argsParser.go
+++ argsParser.go
@@ -11,28 +11,33 @@ func ArgsParser(flags *Flags, args *[]string) {
 			flags.Paths = append(flags.Paths, (*args)[i])
 		} else if (*args)[i] == "--" {
 			flags.ModeOnlyPathsActivated = true
-		} else if len((*args)[i]) >= 2 && (*args)[i][0] == '-' && (*args)[i][1] != '-' {
-			splicedArgs := strings.Split((*args)[i], "")
-			for _, flag := range splicedArgs[1:] {
-				switch flag {
-				case "l":
-					flags.Dash_l = true
-				case "R":
-					flags.Dash_R = true
-				case "a":
-					flags.Dash_a = true
-				case "r":
-					flags.Dash_r = true
-				case "t":
-					flags.Dash_t = true
-				case "S":
-					flags.Dash_S = true
-				case "A":
-					flags.Dash_A = true
-				default:
-					log.Fatalf("--%s is not a valid option", flag)
+		} else if len((*args)[i]) >= 2 && (*args)[i][0] == '-' {
+			if (*args)[i][1] != '-' {
+				splicedArgs := strings.Split((*args)[i], "")
+				for _, flag := range splicedArgs[1:] {
+					switch flag {
+					case "l":
+						flags.Dash_l = true
+					case "R":
+						flags.Dash_R = true
+					case "a":
+						flags.Dash_a = true
+					case "r":
+						flags.Dash_r = true
+					case "t":
+						flags.Dash_t = true
+					case "S":
+						flags.Dash_S = true
+					case "A":
+						flags.Dash_A = true
+					default:
+						log.Fatalf("--%s is not a valid option", flag)
+					}
 				}
+			} else {
+				log.Fatalf("%s is not a valid option", (*args)[i])
 			}
+
 		} else {
 			flags.Paths = append(flags.Paths, (*args)[i])
 		}
